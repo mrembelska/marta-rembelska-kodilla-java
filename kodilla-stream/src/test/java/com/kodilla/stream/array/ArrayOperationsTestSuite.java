6bbaf91c2c7f9b79d50a14298c2b34430c996d48
Marta Rembelska <martarembelska@pc46.home>
2018-03-06 21:57:38 +0100

    7.5

---
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -0,0 +1,18 @@
+package com.kodilla.stream.array;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ArrayOperationsTestSuite {
+    @Test
+    public void testGetAverage() {
+        //Given
+        int[] listOfNumbers = {1,2,3,4,5,15,14};
+
+        //When
+        double average = ArrayOperation.getAverage(listOfNumbers);
+
+        //Then
+        Assert.assertEquals(8.29, average, 0.01);
+    }
+}
